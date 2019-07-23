825b7f34419204ef4cc6121b4f6d833f2c5c51dd
Maxim Lobanov <v-malob@microsoft.com>
2019-07-23 19:35:39 +0300

    change test

--- TestAppUITests/TestAppUITests.m
+++ TestAppUITests/TestAppUITests.m
@@ -27,42 +27,22 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)tryUIWebViewOfRowID:(NSString*) rowId {
-    // UI tests must launch the application that they test.
+- (void)testUIWebView {
     XCUIApplication *app = [[XCUIApplication alloc] init];
     [app launch];
-
-    XCUIElement* docLoaded = app.staticTexts[@"Document Loaded!"];
-    NSPredicate* docLoadedExists = [NSPredicate predicateWithFormat:@"exists == true"];
-    
-    [self expectationForPredicate:docLoadedExists evaluatedWithObject:docLoaded handler:nil];
-    
     [app.buttons[@"Misc"] tap];
-    [app.tables.cells[rowId] tap];
+    [app.tables.cells[@"text input row"] tap];
     
-    [self waitForExpectationsWithTimeout:15 handler:nil];
-
-    XCUIElement* firstname = [app.webViews.textFields elementBoundByIndex:0];
-    [firstname tap];
-    [firstname typeText:@"123"];
-
-    NSString* v0 = firstname.value;    // v0    __NSCFString *    @"\U0000fffc"    0x0000600002d19a00
-    XCTAssertEqualObjects(v0, @"123"); // ((v0) equal to (@"123")) failed: ("￼") is not equal to ("123")
+    XCUIElement* textField = [app.textFields elementBoundByIndex:0];
+    [textField tap];
+    [textField typeText:@"123"];
+    XCUIElement *q = app.textFields[@"text field"];
+    NSString* v = q.value;
+    NSLog(@"%@", v);
+    XCTAssertEqualObjects(v, @"123");
+    
 }
 
-- (void)testUIWebView {
-    [self tryUIWebViewOfRowID: @"uiwebview row"];
-}
-
-
-- (void)testWKWebView {
-    [self tryUIWebViewOfRowID: @"wkwebview row"];
-}
-
-
-- (void)testSafariWebControllerWebView {
-    [self tryUIWebViewOfRowID: @"safari web controller row"];
-}
 
 @end
 
