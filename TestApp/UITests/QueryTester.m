eb9b38b9a4862bec8550a7c8dd1ccf6a021c82e7
Ivan Nosar <v-ivnosa@microsoft.com>
2019-09-20 15:30:29 +0300

    UI test

--- TestApp/UITests/QueryTester.m
+++ TestApp/UITests/QueryTester.m
@@ -25,7 +25,7 @@
     [app launch];
 
     [self openConTextPage: app];
-    
+
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"identifier == \"text field\" OR accessibilityIdentifier == \"text field\" OR label == \"text field\" OR accessibilityLabel == \"text field\" OR title == \"text field\" OR value == \"text field\" OR placeholderValue == \"text field\""];
     
     XCUIElementQuery *query = [app.textFields matchingPredicate: predicate];
@@ -34,47 +34,9 @@
     
     XCTAssertNotNil(field);
     
-    NSString *enteredValue = @"qwertyuiop";
-    [field doubleTap];
-    [field typeText:enteredValue];
-    
     NSLog(@"Value = %@", field.value);
-    XCTAssertEqual(field.value, enteredValue);
     NSLog(@"Placeholder = %@", field.placeholderValue);
     NSLog(@"Identifier = %@", field.identifier);
-//    NSLog(@"Type = %@", field.elementType);
-    
-    
-//    id applicationQuery = [XCUIApplication cbxQuery:self];
-//
-//    [XCUIApplication cbxResolveApplication:self
-//                          applicationQuery:applicationQuery];
-//
-//    Class klass = NSClassFromString(@"XCApplicationQuery");
-//    SEL selector = NSSelectorFromString(@"descendantsMatchingType:");
-//
-//    NSMethodSignature *signature;
-//    signature = [klass instanceMethodSignatureForSelector:selector];
-//    NSInvocation *invocation;
-//
-//    invocation = [NSInvocation invocationWithMethodSignature:signature];
-//    invocation.target = applicationQuery;
-//    invocation.selector = selector;
-//
-//    XCUIElementType elementType = XCUIElementTypeAny;
-//    [invocation setArgument:&elementType atIndex:2];
-//
-//    void *buffer = nil;
-//    [invocation invoke];
-//    [invocation getReturnValue:&buffer];
-//    XCUIElementQuery *query = (__bridge XCUIElementQuery *)buffer;
-//
-//
-//    for (QuerySpecifier *specifier in self.queryConfiguration.selectors) {
-//        query = [specifier applyToQuery:query];
-//    }
-//
-//    NSArray <XCUIElement *> *elements = [query allElementsBoundByIndex];
 }
 
 @end
