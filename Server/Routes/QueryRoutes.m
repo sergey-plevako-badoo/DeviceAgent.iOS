790f0d9209cf1db3de0ae465e8dd6b128ba70975
Sergey Plevako <sergey.plevako@corp.badoo.com>
2020-04-21 19:46:12 +0100

    Added new query API providing the bundle_ids

--- Server/Routes/QueryRoutes.m
+++ Server/Routes/QueryRoutes.m
@@ -59,6 +59,36 @@
               NSDictionary *json = [JSONUtils snapshotOrElementToJSON:el];
               [results addObject:json];
           }
+          
+          [response respondWithJSON:@{@"result" : results}];
+        }],
+      
+      [CBXRoute post:endpoint(@"/query_all", 1.0) withBlock:^(RouteRequest *request,
+                                                          NSDictionary *body,
+                                                          RouteResponse *response) {
+          // [[SpringBoard application] handleAlertsOrThrow];
+          QueryConfiguration *config;
+          config = [QueryConfigurationFactory configWithJSON:body
+                                                   validator:[Query validator]];
+          Query *query = [QueryFactory queryWithQueryConfiguration:config];
+          
+          NSArray* bundleIds = [request.params[CBX_BUNDLE_ID_KEY] componentsSeparatedByString:@","];
+          NSArray <XCUIElement *> *elements= nil;
+          NSMutableArray *results = nil;
+          
+          for (NSString *bundleId in bundleIds) {
+              elements = [query execute: bundleId];
+              
+              /*
+               Format and return the results
+               */
+              results = [NSMutableArray arrayWithCapacity:elements.count];
+              for (XCUIElement *el in elements) {
+                  NSDictionary *json = [JSONUtils snapshotOrElementToJSON:el];
+                  [results addObject:json];
+              }
+          }
+
           [response respondWithJSON:@{@"result" : results}];
       }],
 
